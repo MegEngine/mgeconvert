e364ba3bb92d3ff0dd34f9a28e04b51807cee454
xiongpeng <xiongpeng@megvii.com>
2020-12-16 16:03:30 +0800

    chore(cambricon): build cambricon converter automaticaly

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 import glob
 import os
 import re
+import subprocess
 
-from setuptools import Extension as _Extension
-from setuptools import find_packages, setup
+from setuptools import Extension, find_packages, setup
+from setuptools.command.build_ext import build_ext
 
 with open(os.path.join("./mgeconvert", "version.py")) as f:
     __version_py__ = f.read()
@@ -13,26 +14,55 @@ MEGENGINE_LOWER = re.search(r"MEGENGINE_LOWER = \"(.*)\"", __version_py__).group
 __version__ = re.search(r"__version__ = \"(.*)\"", __version_py__).group(1)
 
 
-class Extension(_Extension):
-    def __init__(self, *a, include_dirs=None, **kw):
-        super(Extension, self).__init__(*a, **kw)
-        self._include_dirs = include_dirs or []
+class CMakeExtension(Extension):
+    def __init__(self, name, source_dir, lib_dir, products):
+        super().__init__(name, sources=[])
+        self.source_dir = source_dir
+        self.lib_dir = os.path.join(source_dir, lib_dir)
+        self.products = products
 
-    """
-    Here we make `include_dirs` an property to make it an deferred attibute,
-    so the `numpy` package can be installed via `setup_requires`, and imported
-    in this property.
-    """
 
-    @property
-    def include_dirs(self):
-        import numpy as np
+class cmake_build_ext(build_ext):
+    def run(self):
+        for ext in self.extensions:
+            try:
+                self.build_cmake(ext)
+            except subprocess.CalledProcessError:
+                continue
 
-        return self._include_dirs + [np.get_include()]
+    def build_cmake(self, ext):
+        source_dir = os.path.abspath(ext.source_dir)
 
-    @include_dirs.setter
-    def include_dirs(self, value):
-        self._include_dirs = value
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+
+        path = os.path.join(source_dir, "swig", "numpy.i")
+        url = "https://raw.githubusercontent.com/numpy/numpy/master/tools/swig/numpy.i"
+        if not os.path.exists(path):
+            subprocess.check_call(["wget", "-P", os.path.dirname(path), url])
+
+        subprocess.check_call(["cmake", source_dir], cwd=self.build_temp)
+        subprocess.check_call(["cmake", "--build", "."], cwd=self.build_temp)
+
+        for product in ext.products:
+            source_file = os.path.join(self.build_temp, product)
+            dest_file = os.path.join(self.build_lib, ext.lib_dir, product)
+            self.copy_file(source_file, dest_file)
+
+
+if os.getenv("USE_CAMBRICON_CONVERTER"):
+    ext_modules = [
+        CMakeExtension(
+            name="_cambriconLib",
+            source_dir="mgeconvert/cambricon_converter",
+            lib_dir="lib/cnlib",
+            products=["_cambriconLib.so", "cambriconLib.py"],
+        )
+    ]
+    cmdclass = {"build_ext": cmake_build_ext}
+else:
+    ext_modules = []
+    cmdclass = {}
 
 
 setup(
@@ -43,6 +73,8 @@ setup(
     author_email="brain-engine@megvii.com",
     url="https://github.com/MegEngine/mgeconvert",
     packages=find_packages(exclude=["test", "test.*"]),
+    ext_modules=ext_modules,
+    cmdclass=cmdclass,
     include_package_data=True,
     setup_requires=["Cython >= 0.20", "numpy"],
     install_requires=["megengine >={}".format(MEGENGINE_LOWER), "numpy"],
