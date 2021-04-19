25f23c2366828e3f4fd46451303febd079f014d7
Peng Xiong <xiongpeng@megvii.com>
2021-04-19 11:45:40 +0800

    feat(mgeconvert): support megengine v1.3.1

--- ci/pytest_tflite.sh
+++ ci/pytest_tflite.sh
@@ -10,6 +10,10 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 sudo python3 -m pip uninstall flatbuffers -y
 sudo python3 -m pip install tensorflow==2.4.0
 
+sudo -H python3 -m pip install -q megengine==1.3.1 -f https://megengine.org.cn/whl/mge.html
+pytest test/test_tflite.py
+sudo -H python3 -m pip uninstall -y megengine
+
 sudo -H python3 -m pip install -q megengine==1.2.0 -f https://megengine.org.cn/whl/mge.html
 pytest test/test_tflite.py
 sudo -H python3 -m pip uninstall -y megengine
