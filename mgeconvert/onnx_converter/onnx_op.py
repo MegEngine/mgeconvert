fix the average unpool

mgeconvert/onnx_converter/onnx_op.py
@@ -523,13 +523,13 @@ class Conv2DBackwardFilterConverter(OperatorBaseConverter):
             grad_slice_axis = outputs[0] + "_grad_weight_slice_axis"
 
             grad_slice_begin_source = onnx.helper.make_tensor_value_info(
-                grad_slice_begin, mge2onnx_dtype_mapping[np.int64], (1,)
+                grad_slice_begin, mge2onnx_dtype_mapping[np.int64], (2,)
             )
             grad_slice_end_source = onnx.helper.make_tensor_value_info(
-                grad_slice_end, mge2onnx_dtype_mapping[np.int64], (1,)
+                grad_slice_end, mge2onnx_dtype_mapping[np.int64], (2,)
             )
             grad_slice_axis_source = onnx.helper.make_tensor_value_info(
-                grad_slice_axis, mge2onnx_dtype_mapping[np.int64], (1,)
+                grad_slice_axis, mge2onnx_dtype_mapping[np.int64], (2,)
             )
             grad_slice_begin_param = onnx.numpy_helper.from_array(
                 np.array([0, 0], dtype=np.int64), grad_slice_begin
@@ -670,6 +670,7 @@ class PoolingBackward2DConverter(Pooling2DConverter):
         input_relu_var, input_pooling_var, input_reshape_var = opr.inp_vars
         output_var = opr.out_vars[0]
 
+        print("var0 : {} , var1 : {}  var2: {}, kh: {} , kw :{} , sh :{} sw : {}".format(input_relu_var.shape,input_pooling_var.shape,input_reshape_var.shape, opr.kh,opr.kw,opr.sh,opr.sw))
         if mode == "AveragePool":
             if opr.kh != opr.sh or opr.kw != opr.sw:
                 raise BaseException(
@@ -692,19 +693,25 @@ class PoolingBackward2DConverter(Pooling2DConverter):
                 "Expand", inputs=[inputs[2], new_shape], outputs=[dp_expand],
             )
             nodes.append(dp_expand_node)
-            size_reciprocal = 1 / (opr.kh * opr.kw)
-            mean_name = inputs[2] + "_mean"
-            mean = onnx.helper.make_tensor_value_info(
-                mean_name, mge2onnx_dtype_mapping[input_pooling_var.dtype], (1,)
+
+            const_name = "average_unpool_const_value"
+            const_node_name = inputs[2] + "_average_unpool_mean"
+            const_value = onnx.helper.make_tensor(
+                    name = const_name,
+                    data_type = mge2onnx_dtype_mapping[input_pooling_var.dtype],
+                    dims=(1,),
+                    vals=np.array([opr.kw*opr.kh]).astype(input_pooling_var.dtype),
+                    )
+            const_node = onnx.helper.make_node(
+                    'Constant',
+                    inputs=[],
+                    outputs=[const_node_name],
+                    value=const_value,
             )
-            mean_value = onnx.numpy_helper.from_array(
-                np.array(size_reciprocal, dtype=input_pooling_var.dtype), mean_name
-            )
-            self._net_sources.append(mean)
-            self._parameters.append(mean_value)
+            nodes.append(const_node)
 
             mat_mean_node = onnx.helper.make_node(
-                "Mul", inputs=[dp_expand, mean_name], outputs=[outputs[0]],
+                "Div", inputs=[dp_expand, const_node_name], outputs=[outputs[0]],
             )
             nodes.append(mat_mean_node)
             return (nodes, self._net_sources, self._parameters)

test/test_onnx.py
@@ -178,19 +178,18 @@ def test_typecvt():
     reason="MGE file for testing was dumped at version 1.5.0",
 )
 def test_convolutionbackwardfilter():
-    import megengine.utils.comp_graph_tools as cgtools  # pylint: disable=import-outside-toplevel
+    import megengine.utils.comp_graph_tools as cgtools
 
     def infer_mge(x, file):
-        infer_cg = cgtools.GraphInference(file + ".mge")  # pylint: disable=no-member
+        infer_cg = cgtools.GraphInference(file + ".mge")
         y = list(infer_cg.run(x).values())[0]
-        print(y.mean())
         return y
 
     file = os.path.join(os.path.dirname(__file__), "convolution-backward-filter")
     data = np.ones((8, 1, 32, 32), dtype=np.float32)
     mge_result = infer_mge(data, file)
     _test_convert_result(
-        data, file, mge_result, max_error, min_version=8, max_version=8
+        data, file, mge_result, max_error, min_version=8, max_version=10
     )
 
 
@@ -201,23 +200,27 @@ def test_convolutionbackwardfilter():
 @pytest.mark.parametrize("mode", ["max", "avg"])
 def test_pooling_backward(mode):
     net = PoolOpr(mode)
-    mge_result = dump_mge_model(net, net.data, tmp_file)
-    import megengine.utils.comp_graph_tools as cgtools
 
     def infer_mge(x, file):
+        import megengine.utils.comp_graph_tools as cgtools
         infer_cg = cgtools.GraphInference(file + ".mge")
         y = list(infer_cg.run(x).values())[0]
         return y
 
     if mode == "max":
+        dump_mge_model(net, net.data2, tmp_file)
         file = os.path.join(os.path.dirname(__file__), tmp_file)
-        data = np.ones((30, 3, 224, 224), dtype=np.float32)
-        mge_result = infer_mge(data, file)
+        mge_result = infer_mge(net.data2, file)
         _test_convert_result(
-            data, file, mge_result, max_error, min_version=8, max_version=10
+            net.data2, file, mge_result, max_error, min_version=8, max_version=10
         )
     elif mode == "avg":
-        return
+        dump_mge_model(net, net.data1, tmp_file)
+        file = os.path.join(os.path.dirname(__file__), tmp_file)
+        mge_result = infer_mge(net.data1, file)
+        _test_convert_result(
+            net.data1, file, mge_result, max_error, min_version=8, max_version=10
+        )
 
 
 @pytest.mark.parametrize(
@@ -267,6 +270,28 @@ def test_resblock():
     converter = OnnxConverter(net, opset_version=10, graph_name="graph")
     model = converter.convert()
 
+    def infer_mge(x,y,file):
+        import megengine.utils.comp_graph_tools as cgtools
+        infer_cg = cgtools.GraphInference(file)
+        y = list(infer_cg.run(x,y).values())[0]
+        # print(y.mean())
+        return y
+
+    file = os.path.join(os.path.dirname(__file__), resnet)
+    data = np.random.randn(1, 64,2,2).astype("float32")
+    label = np.random.randn(1,1000).astype("float32")
+    mge_result = infer_mge(data,label,file)
+
+    with open(tmp_file + ".onnx", "wb") as fout:
+        fout.write(model.SerializeToString())
+    onnx_net = ort.InferenceSession(tmp_file + ".onnx")
+    input_name = onnx_net.get_inputs()[0].name
+    label_name = onnx_net.get_inputs()[1].name
+    pred_onx = onnx_net.run(None, {input_name: data , label_name: label})[0]
+    assert pred_onx.shape == mge_result.shape
+    assert pred_onx.dtype == mge_result.dtype
+    assert np.allclose(pred_onx, mge_result, atol=1e-4)
+
 
 def test_resnet():
     if megengine.__version__ < "1.4.0":
@@ -274,3 +299,25 @@ def test_resnet():
     net = TopologyNetwork(resnet)
     converter = OnnxConverter(net, opset_version=10, graph_name="graph")
     model = converter.convert()
+
+    def infer_mge(x,y,file):
+        import megengine.utils.comp_graph_tools as cgtools
+        infer_cg = cgtools.GraphInference(file)
+        y = list(infer_cg.run(x,y).values())[0]
+        return y
+
+    file = os.path.join(os.path.dirname(__file__), resnet)
+    data = np.random.randn(1, 3, 224, 224).astype("float32")
+    label = np.random.randn(1,1000).astype("float32")
+    mge_result = infer_mge(data,label,file)
+
+    with open(tmp_file + ".onnx", "wb") as fout:
+        fout.write(model.SerializeToString())
+    onnx_net = ort.InferenceSession(tmp_file + ".onnx")
+    input_name = onnx_net.get_inputs()[0].name
+    label_name = onnx_net.get_inputs()[1].name
+    pred_onx = onnx_net.run(None, {input_name: data , label_name: label})[0]
+    assert pred_onx.shape == mge_result.shape
+    assert pred_onx.dtype == mge_result.dtype
+    assert np.allclose(pred_onx, mge_result, atol=1e-4)
+

test/utils.py
@@ -122,9 +122,10 @@ class PoolOpr(M.Module):
     def __init__(self, mode):
         super().__init__()
         self.mode = mode
-        self.data = np.random.random((30, 3, 224, 224)).astype(np.float32)
-        self.maxpool = M.pooling.MaxPool2d(kernel_size=3, stride=2, padding=2)
-        self.avgpool = M.pooling.AvgPool2d(kernel_size=3, stride=2, padding=2)
+        self.data1 = np.random.random((1,512,7,7)).astype(np.float32)
+        self.data2 = np.random.random((1,64,112,112)).astype(np.float32)
+        self.maxpool = M.pooling.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.avgpool = M.pooling.AvgPool2d(kernel_size=7, stride=7, padding=0)
 
     def forward(self, x):
         return getattr(self, self.mode + "pool")(x)