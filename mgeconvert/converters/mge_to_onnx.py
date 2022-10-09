f55a48ff5673aebb678a9030f7fb1de5b8205f6c
CaoWGG <cwgnlfd@163.com>
2022-10-09 18:55:19 +0800

    remove license header

--- mgeconvert/converters/mge_to_onnx.py
+++ mgeconvert/converters/mge_to_onnx.py
@@ -1,10 +1,3 @@
-# MegEngine is Licensed under the Apache License, Version 2.0 (the "License")
-#
-# Copyright (c) 2014-2020 Megvii Inc. All rights reserved.
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT ARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 from ..backend.ir_to_onnx import OnnxConverter
 from ..converter_ir.ir_transform import IRTransform, TransformerRule
 from ..frontend.mge_to_ir import MGE_FrontEnd
