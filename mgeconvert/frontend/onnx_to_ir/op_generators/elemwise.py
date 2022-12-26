9874fc69c288d7b65aeec3a511f9e7fdd3b70dca
yeasoon <1695924908@qq.com>
2022-12-26 18:18:45 +0800

    feat(onnx): add squeeze and sum op and fix sofmax op for onnx to mge

--- mgeconvert/frontend/onnx_to_ir/op_generators/elemwise.py
+++ mgeconvert/frontend/onnx_to_ir/op_generators/elemwise.py
@@ -10,6 +10,7 @@ from ....converter_ir.ir_op import (
     SigmoidOpr,
     SoftmaxOpr,
     SqrtOpr,
+    SqueezeOpr,
     SubOpr,
 )
 from .base import OpGenBase, _register_op
@@ -25,7 +26,7 @@ class GenMulOpr(OpGenBase):
         self.add_tensors()
 
 
-@_register_op("Add")
+@_register_op("Add", "Sum")
 class GenAddOpr(OpGenBase):
     def __init__(self, node, ir_graph, resolver, opset):
         # pylint: disable=W0612,W0613
@@ -33,6 +34,7 @@ class GenAddOpr(OpGenBase):
 
         self.op = AddOpr()
         self.add_tensors()
+        assert len(self.op.inp_tensors) == 2, "Sum of more than two is not supported"
 
 
 @_register_op("Sub")
@@ -136,8 +138,30 @@ class GenSoftmaxOpr(OpGenBase):
     def __init__(self, node, ir_graph, resolver, opset):
         # pylint: disable=W0612,W0613
         super().__init__(node, ir_graph, resolver)
+        if len(node.attribute) != 0:
+            assert (
+                len(node.attribute) == 1
+            ), "Softmax's attribute should be only 1 when exist"
+            self.axis = node.attribute[0].i
+            self.op = SoftmaxOpr(self.axis)
+            self.add_tensors()
+        else:
+            self.axis = 1
+            self.op = SoftmaxOpr(self.axis)
+            self.add_tensors()
+            inp_shape = self.op.inp_tensors[0].shape
+            assert (
+                len(inp_shape) == 2 and inp_shape[0] == 1
+            ), "Softmax's input shape dims should be only 1 x N when attribute not exist"
 
-        assert len(node.attribute) == 1, "Softmax's attribute should be only 1"
-        self.axis = node.attribute[0].i
-        self.op = SoftmaxOpr(self.axis)
+
+@_register_op("Squeeze")
+class GenSqueezeOpr(OpGenBase):
+    def __init__(self, node, ir_graph, resolver, opset):
+        # pylint: disable=W0612,W0613
+        super().__init__(node, ir_graph, resolver)
+        for attr in node.attribute:
+            if attr.name == "axes":
+                self.axis = tuple(attr.ints)
+        self.op = SqueezeOpr(self.axis)
         self.add_tensors()
